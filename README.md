10e5780de0dd985ea0377db8bbbe99ac94d39221
Matt Wiens <mwiens91@gmail.com>
2017-03-06 19:30:57 -0800

    Start script
    Resetting sudo timestamp doesn't work

---
+++ README.md
@@ -0,0 +1,6 @@
+state of trance sort
+ -> take in arguments of folders
+ -> find which drive to use both from /dev/.... and /media/
+ -> rename folder to ASOT number if supplied
+ -> remove cover
+ -> fatsort drive
---
+++ moveASOT.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+#state of trance sort
+# -> take in arguments of folders
+# -> find which drive to use both from /dev/.... and /media/
+# -> rename folder to ASOT number if supplied
+# -> remove cover
+# 0 > move stuff
+# -> fatsort drive
+# mount drive back
+
+import argparse
+import os
+import sys
+
+if __name__ == '__main__':
+
+    # Parse input arguments
+    parser = argparse.ArgumentParser(
+            description="NOTE stuff here",
+            epilog="OTHER STUFF HERE")
+    parser.add_argument("dirs",
+            metavar="ASOT_Episode_Dirs",
+            nargs='+',
+            type=str,
+            help="Relative ASOT directory paths")
+    parser.add_argument("--verbose",
+            help="Give lots of output",
+            action="store_true")
+    args = parser.parse_args()
+
+    # Get root access if we don't have it already
+    euid = os.geteuid()
+    if euid != 0:
+        args = ['sudo', sys.executable] + sys.argv + [os.environ]
+        # Replace currently-running process with root-access process
+        os.execlpe('sudo', *args)
+
+    # Clean-up: reset sudo-timestamp
+    os.system("sudo -k")
